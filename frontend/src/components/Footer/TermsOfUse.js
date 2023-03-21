a9cebe7a06a70db816e18e7b4f00b12d12b7e309
Uma Sowmya Puppala <umasowmya189@gmail.com>
2023-03-21 19:20:14 -0400

    more css changes

---
+++ frontend/src/components/Footer/TermsOfUse.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import CustomNavbar from '../CustomNavbar';
+
+const TermsOfUse = () => {
+  return (
+        <div className="body_wrapper">
+        <CustomNavbar
+        mClass="menu_four hosting_menu"
+        nClass="w_menu m-auto"
+        slogo="sticky_logo"
+        hbtnClass="event_btn"
+      />
+        <div id = "Terms">
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <h1>Terms Of Use</h1>
+                    <p>By accessing and using our website, you agree to be bound by the following terms of use. If you do not agree with any of these terms, please do not use our website. </p>
+                    <ol>
+                        <li>
+                            <strong>1. Informational Purposes Only:</strong>
+                            <p>Our website is intended for informational purposes only. The information provided on our website is not intended to be a substitute for professional medical advice, diagnosis, or treatment. Always seek the advice of your physician or other qualified healthcare provider with any questions you may have regarding a medical condition.</p>
+                            </li>
+                            <li>
+        <strong>2. Eligibility to Donate Blood:</strong>
+        <p>To donate blood, you must meet certain eligibility requirements as determined by the governing bodies and organizations that regulate blood donations. We encourage you to review the eligibility requirements before attempting to donate blood. By accessing and using our website, you represent and warrant that you meet the eligibility requirements to donate blood.</p>
+        </li>
+        <li>
+        <strong>3. Personal Information:</strong>
+        <p>We take your privacy seriously and are committed to protecting your personal information. By accessing and using our website, you agree to our privacy policy, which describes how we collect, use, and disclose your personal information.</p>
+        </li>
+        <li>
+        <strong>4. User Conduct:</strong>
+        <p>You are solely responsible for your conduct while using our website. You agree not to:</p>
+        a.Use our website for any unlawful purpose or in violation of these terms of use;<br />
+        b.Interfere with or disrupt the operation of our website or any servers or networks used to make our website available;<br />
+        c.Impersonate any person or entity or falsely state or misrepresent your affiliation with any person or entity;<br />
+        d.Upload, post, transmit, or otherwise make available any content that is defamatory, obscene, threatening, or otherwise offensive;<br />
+        e.Use any device, software, or other technology to interfere with or attempt to interfere with the proper functioning of our website;<br />
+        f.Engage in any activity that could damage, disable, or overburden our website or any servers or networks used to make our website available.<br />
+        </li>
+        <li>
+        <strong>5. Intellectual Property</strong>
+        <p>All content on our website, including but not limited to text, graphics, logos, images, and software, is the property of the blood donation organization and is protected by copyright and other intellectual property laws. You may not copy, modify, distribute, transmit, display, perform, reproduce, or publish any part of our website without our prior written consent.</p>
+        </li>
+        <li>
+        <strong>6. Disclaimer of Warranties</strong>
+        <p>Our website is provided on an 'as is' and 'as available' basis. We do not warrant that our website will be uninterrupted or error-free or that any defects will be corrected. We make no representations or warranties of any kind, express or implied, regarding the use or the results of our website in terms of its correctness, accuracy, reliability, or otherwise.</p>
+        </li>
+        <li>
+        <strong>7. Limitation of Liability</strong>
+        <p>To the fullest extent permitted by applicable law, we shall not be liable for any direct, indirect, incidental, special, or consequential damages arising out of or in any way connected with the use of our website or the inability to use our website, even if we have been advised of the possibility of such damages.</p>
+        </li>
+        <li>
+        <strong>8. Modifications to Terms of Use</strong>
+        <p>We reserve the right to modify these terms of use at any time without prior notice. Your continued use of our website following any such modifications constitutes your acceptance of the modified terms of use.</p>
+        </li>
+        <li>
+        <strong>9. Governing Law</strong>
+        <p>These terms of use shall be governed by and construed in accordance with the laws of [insert governing law]. Any disputes arising out of or in connection with these terms of use shall be subject to the exclusive jurisdiction of the courts of [insert jurisdiction].</p>
+        </li>
+        </ol>
+        <br />
+        <p>By accessing and using our website, you agree to be bound by the following terms of use. If you do not agree with any of these terms, please do not use our website.</p>
+        <p>Thank you for reading and abiding by our terms of use. If you have any questions or concerns, please contact us."</p>
+        <br />
+        <br />
+        <br />
+        </div>
+    </div>
+  );
+};
+export default TermsOfUse;
