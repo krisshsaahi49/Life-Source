26bde4b49066e9c37183ce99fe7349b21da5ace2
Uma Sowmya Puppala <umasowmya189@gmail.com>
2023-04-25 17:12:06 -0400

    Donor page modifications

--- frontend/src/Pages/DonorMainPage.js
+++ frontend/src/Pages/DonorMainPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import DonorProfile from "./DonorProfile";
-import "../assets/recipientMain.css";
+import "../assets/donorMain.css";
 
 const DonorMainPage = () => {
   const [requests, setRequests] = useState([]); // State for storing incoming requests
@@ -30,32 +30,30 @@ const DonorMainPage = () => {
   };
 
   return (
-    <>
      <div id="donor" className="wrapper">
-      <h2>Welcome to Donor Page</h2>
+      <h2 className="donor-welcome-header">Donor Page</h2>
         <DonorProfile/>
-      <h2>Donor Page</h2>
+      <h2 className="donor-page-header">Recipient Requests</h2>
       {requests.length > 0 ? (
         <div>
-          <h3>Incoming Requests:</h3>
-          <ul>
+          <h3 className="incoming-requests-header">Incoming Requests:</h3>
+          <ul className="incoming-requests-list">
             {requests.map((recipientName, index) => (
-              <li key={index}>
+              <li key={index} className="incoming-requests-item">
                 {recipientName}
-                <button onClick={() => handleAcceptRequest(recipientName)}>Accept</button>
-                <button onClick={() => handleRejectRequest(recipientName)}>Reject</button>
+                <button className="accept-button" onClick={() => handleAcceptRequest(recipientName)}>Accept</button>
+                <button className="reject-button" onClick={() => handleRejectRequest(recipientName)}>Reject</button>
               </li>
             ))}
           </ul>
         </div>
       ) : (
-        <p>No incoming requests.</p>
+        <p className="no-requests-text">No incoming requests.</p>
       )}
-      <button onClick={() => handleIncomingRequest("Recipient1")}>
-        Simulate Incoming Request
+      <button className="simulate-button" onClick={() => handleIncomingRequest("Recipient1")}>
+        Show Incoming Request
       </button>
     </div>
-    </>
   );
 };
 
